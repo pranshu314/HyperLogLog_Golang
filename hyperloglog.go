d3e5ff7841e07f5d4cb3831c5b5a04dc578321ad
Pranshu <pranshu2.71828@gmail.com>
2024-12-06 11:10:41 +0530

    Add: generate_hash function

--- hyperloglog.go
+++ hyperloglog.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"crypto/sha1"
 	"fmt"
 )
+
+func generate_hash(text string) []byte {
+	sha := sha1.New()
+	sha.Write([]byte(text))
+	hash := sha.Sum(nil)
+	return hash
+}
 
 func main() {
 	fmt.Println("Hello, World!")
